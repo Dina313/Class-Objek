0e1ef02bde92ca5b0b591c62d9bb04c35eb61217
DinaRahma <dinarahmaa354@gmail.com>
2019-03-19 12:15:34 +0700

    latihan membuat Class

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_objek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            // mengeset nilai properties objek saya dan kamu
+            saya.Nim = "18.11.2372";
+            saya.Nama = "Dina Rahma Novita";
+            saya.Ipk = 3.75f;
+
+            kamu.Nim = "18.11.2342";
+            kamu.Nama = "Vicka Rizki sunflora";
+            kamu.Ipk = 3.92f;
+
+            // memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
